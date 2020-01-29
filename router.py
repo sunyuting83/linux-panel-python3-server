fedc17f09a5220d4c708e59e8bf62003032708cd
sunyuting <sleepsun123@163.com>
2020-01-29 16:29:42 +0800

    add method for route

--- router.py
+++ router.py
@@ -2,47 +2,46 @@ from lib import route, response, request, static_file, getSys, getMem, getHtop, 
 import os
 
 # 首页403
-@route('/')
+@route('/', method=['GET','POST','PUT','DELETE','OPTIONS'])
 def index():
   return abort(403)
 
 # 防止爬虫
-@route('/robots.txt')
+@route('/robots.txt', method='GET')
 def robots():
-	# print app.static_folder
 	response.content_type = 'text/plain; charset=UTF-8'
 	return '''User-agent: *
 Disallow: /'''
 
-@route('/favicon.ico')
+@route('/favicon.ico', method='GET')
 def favicon():
   response.content_type = 'application/x-ico'
   return static_file('favicon.ico', root=os.path.join(os.getcwd(), 'static'), mimetype = 'application/x-ico')
 
 
 # CPU,硬盘,系统信息
-@route('/getSystem')
+@route('/getSystem', method='GET')
 @login_required
 def getsystem():
   system = getSys()
   return return_Json(system)
 
 # 内存,网络
-@route('/getSync')
+@route('/getSync', method='GET')
 @login_required
 def getsync():
   mem = getMem()
   return return_Json(mem)
 
 # 进程列表
-@route('/htop')
+@route('/htop', method='GET')
 @login_required
 def gethtop():
   top = getHtop()
   return return_Json(top)
 
 # 目录获取
-@route('/getPath')
+@route('/getPath', method='GET')
 @login_required
 def getpath():
   path = request.query.path
